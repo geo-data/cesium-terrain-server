cd4e9a4a3a389f9c3023ecb2d9dfb8635cfb3aaa
Homme Zwaagstra <hrz@geodata.soton.ac.uk>
2014-12-10 14:37:34 +0000

    Enable a Docker image to be built if there is no local source archive
    If a local archive containing the project source is present, it will
    be used when building the image.  If no source archive is found,
    however, the source is cloned from GitHub.

--- docker/install-terrain-server.sh
+++ docker/install-terrain-server.sh
@@ -11,7 +11,12 @@ CTS_DIR=${GOPATH}/src/github.com/geo-data/cesium-terrain-server
 # Extract the terrain server code
 mkdir -p $CTS_DIR || exit 1
 cd $CTS_DIR || exit 1
-tar -xzvf /tmp/local/cesium-terrain-server.tar.gz || exit 1
+
+if [ ! -f /tmp/local/cesium-terrain-server.tar.gz ]; then
+    git clone https://github.com/geo-data/cesium-terrain-server.git . || exit 1
+else
+    tar -xzvf /tmp/local/cesium-terrain-server.tar.gz || exit 1
+fi
 
 # Build and install the server
 make server || exit 1
