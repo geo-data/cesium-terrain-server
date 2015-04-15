e4b0852ac782339d23b5e6b4c3a90f460d0b7492
Homme Zwaagstra <hrz@geodata.soton.ac.uk>
2015-04-15 14:11:40 +0100

    Ensure the docker image is built from a versioned source code archive.

--- docker/install-terrain-server.sh
+++ docker/install-terrain-server.sh
@@ -6,17 +6,21 @@
 
 export GOPATH=/tmp/go
 export PATH=${GOPATH}/bin:$PATH
-CTS_DIR=${GOPATH}/src/github.com/geo-data/cesium-terrain-server
 
-# Extract the terrain server code
+# If a local source code archive does not exist, get it from GitHub.
+checkout=`cat /tmp/cts-checkout.txt`
+archive="/tmp/local/cesium-terrain-server-${checkout}.tar.gz"
+if [ ! -f $archive ]; then
+    wget --no-verbose -O $archive "https://github.com/geo-data/cesium-terrain-server/archive/${checkout}.tar.gz" || exit 1
+fi
+
+# Set up the source directory
+CTS_DIR=${GOPATH}/src/github.com/geo-data/cesium-terrain-server
 mkdir -p $CTS_DIR || exit 1
 cd $CTS_DIR || exit 1
 
-if [ ! -f /tmp/local/cesium-terrain-server.tar.gz ]; then
-    git clone https://github.com/geo-data/cesium-terrain-server.git . || exit 1
-else
-    tar -xzvf /tmp/local/cesium-terrain-server.tar.gz || exit 1
-fi
+# Extract the terrain server code
+tar --strip-components=1 -xzf $archive || exit 1
 
 # Build and install the server
 make bin/cesium-terrain-server || exit 1
